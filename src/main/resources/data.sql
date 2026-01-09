080206f2e74ae13d4c4d9c60b7f4956ad3d9b07d
bhs <littleconan@hanmail.net>
2026-01-10 00:23:58 +0900

    second

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,9 +21,9 @@ VALUES ('user02', '1234', '김철수', 'kim@test.com', '010-2222-2222');
 
 -- 2. Board 데이터 (member_id 컬럼에 숫자를 넣어야 에러가 안 납니다)
 -- 홍길동(1번)이 쓴 글
-INSERT INTO TB_BOARD (title, contents, member_id, hits, delete_yn, created_date, modified_date)
+INSERT INTO TB_BOARD (title, contents, member_id, hits, deleteYn, createdDate, modifiedDate)
 VALUES ('첫 번째 게시글입니다', '안녕하세요, 반가워요.', 1, 0, 'N', NOW(), NOW());
 
 -- 김철수(2번)가 쓴 글
-INSERT INTO TB_BOARD (title, contents, member_id, hits, delete_yn, created_date, modified_date)
+INSERT INTO TB_BOARD (title, contents, member_id, hits, deleteYn, createdDate, modifiedDate)
 VALUES ('두 번째 질문있어요', 'JPA 조인이 어렵네요.', 2, 5, 'N', NOW(), NOW());
